94d3522b8815263f38d5ec0bf7f891d3e641bda8
WangTieX <1575374226@qq.com>
2020-09-08 21:36:09 +0800

    1.2  将app.vue中的数据抽离为组件

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,17 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <TabBar>
-      <TabBarItem path="/home">
-        <span class="iconfont icon-shouye" slot="item-icon"></span>
-        <div slot="item-text">首页</div>
-      </TabBarItem>
-      <TabBarItem path="/category">
-        <span class="iconfont icon-classify-on" slot="item-icon"></span>
-        <div slot="item-text">分类</div>
-      </TabBarItem>
-      <TabBarItem path="/cart">
-        <span class="iconfont icon-gouwuche" slot="item-icon"></span>
-        <div slot="item-text">购物车</div>
-      </TabBarItem>
-      <TabBarItem path="/profile">
-        <span class="iconfont icon-wode" slot="item-icon"></span>
-        <div slot="item-text">我的</div>
-      </TabBarItem>
-    </TabBar>
+    <MainTabBar></MainTabBar>
   </div>
 </template>
 
 <script>
-// 引入底部tabbar组件
-import TabBar from "./components/tabbar/TabBar";
-// 引入底部tabbar-item组件
-import TabBarItem from "./components/tabbar/TabBarItem";
+import MainTabBar from "./components/MainTabBar";
 
 export default {
   name: "App",
   components: {
-    TabBar,
-    TabBarItem
+    MainTabBar
   }
 };
 </script>
